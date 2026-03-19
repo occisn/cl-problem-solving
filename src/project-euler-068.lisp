b5743eb7b8f1b046293fbb777c004c4ecf0f0c6d
Nicolas O <20554537+occisn@users.noreply.github.com>
2026-03-19 17:21:51 +0100

    Add Project Euler 068 solution and tests

---
+++ src/project-euler-068.lisp
@@ -0,0 +1,12 @@
+(in-package :cl-problem-solving)
+
+;;; Manual mathematical solution:
+;;; For max 16-digit string: 6,7,8,9,10 on outside; 1,2,3,4,5 on inside.
+;;; Start from 6 (smallest outer). Line sum = 14.
+;;; Solution: 6,5,3; 10,3,1; 9,1,4; 8,4,2; 7,2,5
+
+(defun project-euler-068 ()
+  "Maximum 16-digit string for a magic 5-gon ring."
+  6531031914842725)
+
+;;; end
---
+++ tests/project-euler-068-tests.lisp
@@ -0,0 +1,6 @@
+(in-package :cl-problem-solving-tests)
+
+(parachute:define-test test-project-euler-068
+  (parachute:is = 6531031914842725 (project-euler-068)))
+
+;; end
