3a109c0b05173f5806746f4d27670655f1ae8d5f
Nicolas O <20554537+occisn@users.noreply.github.com>
2026-03-19 17:20:14 +0100

    Add Project Euler 021 solution and tests

---
+++ src/project-euler-021.lisp
@@ -0,0 +1,29 @@
+(in-package :cl-problem-solving)
+
+(defun pe021--sum-of-proper-divisors (n)
+  "Sum of proper divisors of n (all divisors less than n)."
+  (declare (type fixnum n))
+  (if (= n 1)
+      1
+      (let* ((isqrt (isqrt n))
+             (perfect-square-p (= n (the fixnum (* isqrt isqrt)))))
+        (declare (type fixnum isqrt))
+        (the fixnum
+             (+ (loop for i of-type fixnum from 2 to isqrt
+                      when (zerop (mod n i))
+                        sum (the fixnum (+ i (the fixnum (floor n i)))) of-type fixnum)
+                1
+                (if perfect-square-p (- isqrt) 0))))))
+
+(defun project-euler-021 (&optional (maxi 10000))
+  "Sum of all amicable numbers under maxi."
+  (declare (type fixnum maxi))
+  (let ((sum 0))
+    (declare (type fixnum sum))
+    (loop for a of-type fixnum from 2 below maxi
+          for b of-type fixnum = (pe021--sum-of-proper-divisors a)
+          when (and (/= a b) (= a (pe021--sum-of-proper-divisors b)))
+            do (incf sum a))
+    sum))
+
+;;; end
---
+++ tests/project-euler-021-tests.lisp
@@ -0,0 +1,6 @@
+(in-package :cl-problem-solving-tests)
+
+(parachute:define-test test-project-euler-021
+  (parachute:is = 31626 (project-euler-021)))
+
+;; end
