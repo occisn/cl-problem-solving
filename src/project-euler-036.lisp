d2be5a4962dd1ebd86cedcde96c41617e90c3e67
Nicolas O <20554537+occisn@users.noreply.github.com>
2026-03-19 17:20:42 +0100

    Add Project Euler 036 solution and tests

---
+++ src/project-euler-036.lisp
@@ -0,0 +1,40 @@
+(in-package :cl-problem-solving)
+
+(declaim (inline pe036--reverse-number))
+(defun pe036--reverse-number (n)
+  "Reverse the digits of fixnum n."
+  (declare (type fixnum n))
+  (let ((rev 0))
+    (declare (type fixnum rev))
+    (loop for m of-type fixnum = n then (the fixnum (floor m 10))
+          while (> m 0)
+          do (setf rev (the fixnum (+ (the fixnum (* rev 10)) (the fixnum (mod m 10))))))
+    rev))
+
+(declaim (inline pe036--palindrome-p))
+(defun pe036--palindrome-p (n)
+  "Check if fixnum n is a decimal palindrome."
+  (declare (type fixnum n))
+  (= n (pe036--reverse-number n)))
+
+(declaim (inline pe036--binary-palindrome-p))
+(defun pe036--binary-palindrome-p (n)
+  "Check if fixnum n is a binary palindrome."
+  (declare (type fixnum n))
+  (let ((rev 0) (m n))
+    (declare (type fixnum rev m))
+    (loop while (> m 0)
+          do (setf rev (the fixnum (+ (the fixnum (ash rev 1)) (the fixnum (logand m 1)))))
+             (setf m (ash m -1)))
+    (= n rev)))
+
+(defun project-euler-036 ()
+  "Sum of all numbers below one million palindromic in both base 10 and base 2."
+  (let ((sum 0))
+    (declare (type fixnum sum))
+    (loop for n of-type fixnum from 1 to 999999
+          when (and (pe036--palindrome-p n) (pe036--binary-palindrome-p n))
+            do (incf sum n))
+    sum))
+
+;;; end
---
+++ tests/project-euler-036-tests.lisp
@@ -0,0 +1,6 @@
+(in-package :cl-problem-solving-tests)
+
+(parachute:define-test test-project-euler-036
+  (parachute:is = 872187 (project-euler-036)))
+
+;; end
