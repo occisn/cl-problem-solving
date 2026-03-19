0ae8ad1d10f76591e2beb8aad7534b6bdd298ea1
Nicolas O <20554537+occisn@users.noreply.github.com>
2026-03-19 17:20:08 +0100

    Add Project Euler 017 solution and tests

---
+++ src/project-euler-017.lisp
@@ -0,0 +1,32 @@
+(in-package :cl-problem-solving)
+
+;;; Uses CL's built-in ~R format directive for English words,
+;;; with British "and" convention for hundreds.
+
+(defun pe017--integer->english (n)
+  "Convert integer to English text (British usage with 'and').
+For instance: 342 --> \"three hundred and forty-two\"."
+  (declare (type fixnum n))
+  (cond ((< n 100) (format nil "~R" n))
+        ((zerop (mod n 100)) (format nil "~R" n))
+        (t (format nil "~R and ~R" (- n (mod n 100)) (mod n 100)))))
+
+(defun pe017--count-letters (n)
+  "Count letters (no spaces, no hyphens) in the English representation of n."
+  (declare (type fixnum n))
+  (let ((str (pe017--integer->english n)))
+    (declare (type simple-string str))
+    (the fixnum (- (length str)
+                   (the fixnum (count #\space str))
+                   (the fixnum (count #\- str))))))
+
+(defun project-euler-017 (&optional (n 1000))
+  "Total number of letters used when writing 1 to n in English words."
+  (declare (type fixnum n))
+  (let ((sum 0))
+    (declare (type fixnum sum))
+    (loop for i of-type fixnum from 1 to n
+          do (incf sum (pe017--count-letters i)))
+    sum))
+
+;;; end
---
+++ tests/project-euler-017-tests.lisp
@@ -0,0 +1,9 @@
+(in-package :cl-problem-solving-tests)
+
+(parachute:define-test test-project-euler-017
+  (parachute:is = 23 (pe017--count-letters 342))
+  (parachute:is = 20 (pe017--count-letters 115))
+  (parachute:is = 19 (project-euler-017 5))
+  (parachute:is = 21124 (project-euler-017)))
+
+;; end
