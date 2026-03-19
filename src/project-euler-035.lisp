d5c7873be376a8b7455d203122d531d19fe0e756
Nicolas O <20554537+occisn@users.noreply.github.com>
2026-03-19 17:20:40 +0100

    Add Project Euler 035 solution and tests

---
+++ src/project-euler-035.lisp
@@ -0,0 +1,38 @@
+(in-package :cl-problem-solving)
+
+(declaim (inline pe035--nb-digits))
+(defun pe035--nb-digits (n)
+  "Number of digits in positive fixnum n."
+  (declare (type fixnum n))
+  (loop for d of-type fixnum from 1
+        for m of-type fixnum = 10 then (the fixnum (* m 10))
+        when (< n m) do (return d)))
+
+(defun pe035--all-rotations-prime-p (n sieve ceiling)
+  "Check if all digit rotations of n are prime (using precomputed sieve)."
+  (declare (type fixnum n ceiling)
+           (type (simple-array bit (*)) sieve))
+  (let ((pow (the fixnum (expt 10 (the fixnum (- (pe035--nb-digits n) 1))))))
+    (declare (type fixnum pow))
+    (loop for r of-type fixnum = n then (the fixnum (+ (the fixnum (* (the fixnum (mod r 10)) pow))
+                                                       (the fixnum (floor r 10))))
+          repeat (pe035--nb-digits n)
+          always (and (< r ceiling) (zerop (sbit sieve r))))))
+
+(defun project-euler-035 (&optional (ceiling 1000000))
+  "Count circular primes below ceiling using sieve."
+  (declare (type fixnum ceiling))
+  (let ((sieve (make-array ceiling :element-type 'bit :initial-element 0)))
+    (loop for i of-type fixnum from 2 below ceiling
+          when (zerop (sbit sieve i))
+            do (loop for j of-type fixnum from (the fixnum (* i i)) below ceiling by i
+                     do (setf (sbit sieve j) 1)))
+    (let ((count 0))
+      (declare (type fixnum count))
+      (loop for n of-type fixnum from 2 below ceiling
+            when (and (zerop (sbit sieve n))
+                      (pe035--all-rotations-prime-p n sieve ceiling))
+              do (incf count))
+      count)))
+
+;;; end
---
+++ tests/project-euler-035-tests.lisp
@@ -0,0 +1,6 @@
+(in-package :cl-problem-solving-tests)
+
+(parachute:define-test test-project-euler-035
+  (parachute:is = 55 (project-euler-035)))
+
+;; end
