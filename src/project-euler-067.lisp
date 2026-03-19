06ee98768426549fa12fd2db75135aee926ddbf7
Nicolas O <20554537+occisn@users.noreply.github.com>
2026-03-19 17:21:47 +0100

    Add Project Euler 067 solution and tests

---
+++ src/project-euler-067.lisp
@@ -0,0 +1,20 @@
+(in-package :cl-problem-solving)
+
+;;; Same algorithm as PE018 (bottom-up iteration) but with a 100-row triangle from file.
+
+(defun project-euler-067 ()
+  "Maximum path sum from top to bottom of the 100-row triangle."
+  (let* ((input-file (merge-pathnames "project-euler-input-files/p067_triangle.txt"
+                                      (asdf:system-source-directory "cl-problem-solving")))
+         (triangle (with-open-file (stream input-file)
+                     (loop for raw-line = (read-line stream nil)
+                           while raw-line
+                           for line of-type simple-string = raw-line
+                           collect (mapcar #'parse-integer
+                                           (loop for start = 0 then (the fixnum (+ 1 finish))
+                                                 for finish = (position #\Space line :start start)
+                                                 collecting (subseq line start finish)
+                                                 until (null finish)))))))
+    (project-euler-018 triangle)))
+
+;;; end
---
+++ tests/project-euler-067-tests.lisp
@@ -0,0 +1,6 @@
+(in-package :cl-problem-solving-tests)
+
+(parachute:define-test test-project-euler-067
+  (parachute:is = 7273 (project-euler-067)))
+
+;; end
