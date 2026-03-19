2dbe8c8e83f639f03a0a564023335a44c037fe69
Nicolas O <20554537+occisn@users.noreply.github.com>
2026-03-19 17:21:05 +0100

    Add Project Euler 046 solution and tests

---
+++ src/project-euler-046.lisp
@@ -0,0 +1,16 @@
+(in-package :cl-problem-solving)
+
+(defun project-euler-046 ()
+  "Smallest odd composite that cannot be written as prime + 2*k^2."
+  (labels ((satisfies-conjecture-p (n)
+             (declare (type fixnum n))
+             (loop for k of-type fixnum from 1
+                   for twice-k-sq of-type fixnum = (the fixnum (* 2 (the fixnum (* k k))))
+                   while (< twice-k-sq n)
+                   thereis (pe037--primep (the fixnum (- n twice-k-sq))))))
+    (loop for n of-type fixnum from 9 by 2
+          when (and (not (pe037--primep n))
+                    (not (satisfies-conjecture-p n)))
+            do (return n))))
+
+;;; end
---
+++ tests/project-euler-046-tests.lisp
@@ -0,0 +1,6 @@
+(in-package :cl-problem-solving-tests)
+
+(parachute:define-test test-project-euler-046
+  (parachute:is = 5777 (project-euler-046)))
+
+;; end
