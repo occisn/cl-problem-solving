e8eeb644811e6575847b190f0b26ecbf24f0ed68
Nicolas O <20554537+occisn@users.noreply.github.com>
2026-03-19 17:21:28 +0100

    Add Project Euler 058 solution and tests

---
+++ src/project-euler-058.lisp
@@ -0,0 +1,20 @@
+(in-package :cl-problem-solving)
+
+;;; Walk the spiral diagonals (like PE028) but track the ratio of primes.
+
+(defun project-euler-058 ()
+  "Side length of the square spiral where the prime ratio on diagonals first falls below 10%."
+  (let ((nb-total 1)
+        (nb-primes 0)
+        (current 1))
+    (declare (type fixnum nb-total nb-primes current))
+    (loop for n of-type fixnum from 3 by 2
+          for step of-type fixnum = (the fixnum (- n 1))
+          do (loop repeat 4
+                   do (incf current step)
+                      (incf nb-total)
+                      (when (pe037--primep current) (incf nb-primes)))
+          until (< (the fixnum (* 10 nb-primes)) nb-total)
+          finally (return n))))
+
+;;; end
---
+++ tests/project-euler-058-tests.lisp
@@ -0,0 +1,8 @@
+(in-package :cl-problem-solving-tests)
+
+(parachute:define-test test-project-euler-058
+  ;; commented out in original tests due to being long
+  ;; (parachute:is = 26241 (project-euler-058))
+  )
+
+;; end
