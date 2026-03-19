5e0e4f1a8957bda86cc3e7f9d662847046c27afd
Nicolas O <20554537+occisn@users.noreply.github.com>
2026-03-19 17:21:24 +0100

    Add Project Euler 056 solution and tests

---
+++ src/project-euler-056.lisp
@@ -0,0 +1,19 @@
+(in-package :cl-problem-solving)
+
+(defun project-euler-056 ()
+  "Maximum digital sum of a^b for a,b < 100."
+  (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
+  (let ((best 0))
+    (declare (type fixnum best))
+    (loop for a of-type fixnum from 1 to 99
+          do (loop for b of-type fixnum from 1 to 99
+                   for s of-type fixnum = (let ((n (expt a b)) (sum 0))
+                                            (declare (type integer n) (type fixnum sum))
+                                            (loop while (> n 0)
+                                                  do (incf sum (the fixnum (mod n 10)))
+                                                     (setf n (floor n 10)))
+                                            sum)
+                   when (> s best) do (setf best s)))
+    best))
+
+;;; end
---
+++ tests/project-euler-056-tests.lisp
@@ -0,0 +1,6 @@
+(in-package :cl-problem-solving-tests)
+
+(parachute:define-test test-project-euler-056
+  (parachute:is = 972 (project-euler-056)))
+
+;; end
